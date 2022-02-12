0e5421e95ed79c59e29b727da8ce10add6bac74d
DebarthaChakraborty <debartha2riki@gmail.com>
2022-02-12 17:08:26 +0530

    day 14

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,18 @@
+class Catalogmodel{
+  static final items=[
+    Item(
+        id: 70061,
+        name: "iphone 12 pro",
+        desc: "12th gen",
+        price: 999,
+        color: "#33505a",
+        image: "https://www.apple.com/newsroom/images/product/iphone/standard/Apple_announce-iphone12pro_10132020_big.jpg.large.jpg"
+    )
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -8,14 +21,3 @@ class Item {
 
   Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
 }
-
-final products = [
-  Item(
-    id: "Item70061",
-    name: "iphone 12 pro",
-    desc: "12th gen",
-    price: 999,
-    color: "#33505a",
-    image: "https://www.apple.com/newsroom/images/product/iphone/standard/Apple_announce-iphone12pro_10132020_big.jpg.large.jpg"
-  )
-];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,23 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
+import 'package:flutter_application_1/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final int days = 1;
+
     final String red = "1200";
 
+    loadData() async{
+      var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+      var decodedData = jsonDecode(catalogJson);
+      var productsData = decodedData["products"];
+    }
+
+    @override
+    void initState(){
+      super.initState();
+      loadData();
+    }
+
+
+    final dummyList = List.generate(5, (index) => Catalogmodel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App")
       ),
-      body: Center(
-        child: Container(
-          child: Text("Top $days and with power $red"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index){
+            return ItemWidget(
+              item: dummyList[index],);
+          },
         ),
       ),
       drawer: MyDrawer(),
-    );   
+    );
   }
 }
